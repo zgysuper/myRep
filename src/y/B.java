4c71d62f4bce0294bc923a2aaadb7f3a982dc64e
zgy <zgysuper@163.com>
2014-04-08 16:38:08 +0800

    second

---
+++ src/y/B.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package y;
+
+/**
+ * @version 创建时间：2014年4月8日 下午4:35:38
+ * 类说明
+ */
+/**
+ * @author acer
+ * 
+ */
+public class B {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
