84bfb22c9a620bd4c4a495d6c1c3f331d8bbb7c8
zgy <zgysuper@163.com>
2014-04-08 16:47:50 +0800

    3

--- src/y/B.java
+++ src/y/B.java
@@ -7,17 +7,15 @@ package y;
  * @version 创建时间：2014年4月8日 下午4:35:38
  * 类说明
  */
-/**
- * @author acer
- * 
- */
 public class B {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		String a = "a";
+		String b = "a";
+		
 	}
 
 }
